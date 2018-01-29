a5f887a5f40de4492290bb9c3f72930706d52d3e
Emmanuel Jucaban <EmmanuelJucaban@gmail.com>
2018-01-28 23:58:23 -0800

    Improve performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from './../../../hoc/Auxiliary';
 import Button from './../../UI/Button/Button.js';
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return  (
-        <li key={igKey}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-        </li>
-      );
-    });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        { ingredientSummary }
-      </ul>
-      <p><strong>Total Price: {props.price}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType='Danger' clicked={props.purchaseCancelled}>Cancel</Button>
-      <Button btnType='Success' clicked={props.purchaseContinued}>Continue</Button>
-    </Aux>
-  );
+class OrderSummary extends Component {
+  // This can be a functional component
+  componentWillUpdate() {
+    console.log("[OrderSummary willUpdate");
+  }
+
+  render(){
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return  (
+          <li key={igKey}>
+            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+          </li>
+        );
+      });
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          { ingredientSummary }
+        </ul>
+        <p><strong>Total Price: {this.props.price}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType='Danger' clicked={this.props.purchaseCancelled}>Cancel</Button>
+        <Button btnType='Success' clicked={this.props.purchaseContinued}>Continue</Button>
+      </Aux>
+    );
+  }
 };
 
-export default orderSummary;
+export default OrderSummary;
