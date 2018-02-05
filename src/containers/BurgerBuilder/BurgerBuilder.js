f1109e15036e4a7a4561619c053cdc0fb30ffc23
Emmanuel <emmanueljucaban@gmail.com>
2018-02-04 21:49:35 -0800

    Added axios and added POST request to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux/Auxiliary';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
 import Burger from './../../components/Burger/Burger';
 import BurgerControls from './../../components/Burger/BuildControls/BuildControls';
 import Modal from './../../components/UI/Modal/Modal';
 import OrderSummary from './../../components/Burger/OrderSummary/OrderSummary';
+import axios from './../../axios-order';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -35,7 +36,22 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Emmanuel',
+        address: {
+          street: '123 Alphabet lane',
+          zipCode: 94015,
+          country: 'United States'
+        },
+        deliveryMethod: 'fastest'
+      }
+    };
+    axios.post('/posts.json', order)
+      .then( response => console.log(response))
+      .catch( error => console.log(error));
   }
 
   updatePurchaseState(ingredients) {
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../Aux/Auxiliary';
+import Aux from '../Auxiliary/Auxiliary';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
