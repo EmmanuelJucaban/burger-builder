add74713c1d4fea1dea36dab6d644da48f80dcf9
Emmanuel <emmanueljucaban@gmail.com>
2018-03-04 04:48:34 -0800

    Added continue and cancel handler to checkout

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -14,10 +14,10 @@ const checkoutSummary = (props) => {
       </div>
       <Button
         btnType="Danger"
-        >CANCEL</Button>
+        clicked={props.checkoutCancelled}>CANCEL</Button>
       <Button
         btnType="Success"
-        >CONTINUE</Button>
+        clicked={props.checkoutContinued}>CONTINUE</Button>
     </div>
   );
 };
